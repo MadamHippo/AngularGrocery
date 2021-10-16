d0764c6257fdd647d61eed35d99006c9cd2ad6b8
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2021-10-15 20:08:14 -0700

    Fixed type error, defined name, initalized edit and delete buttons

--- src/app/app-grocery/app.grocery.ts
+++ src/app/app-grocery/app.grocery.ts
@@ -7,13 +7,53 @@ import { Component } from '@angular/core';
  
 export class GroceryComponent {  
   
-  task = "";
-  tasks: Array<any> = [];
- 
+  tasks: any = [];
+  // tasks = Array<any>{};
+  task: any = {
+      name: '',
+      id: 0
+    };
+
   onClick(){
-    this.tasks.push({name: this.task});
-  this.task = '';
+    //this.tasks.push({name: this.task});
+  //this.task = '';
+  if(this.task.id == 0){
+    this.tasks.push({id: (new Date()).getTime(), name: this.task.name, strike: false});
+  }
+  this.task = {
+    name: '',
+    id: 0
+  };
+
+}
+
+  onEdit(item){
+    this.task = item;
+  }
+
+  onDelete(item) {
+    for(var i = 0;i < this.tasks.length; i++){
+      if(item.id == this.tasks[i].id){
+        this.tasks.splice(i,1);
+        break;
+      }
+    }
+    
   }
 
 
-}
+    onStrike(item){
+      for(var i = 0;i < this.tasks.length; i++){
+        if(item.id == this.tasks[i].id){
+          if(this.tasks[i].strike){
+            this.tasks[i].strike = false;
+          }
+          else{
+            this.tasks[i].strike = true;
+          }
+          break;
+        }
+      }
+    }
+
+  }
